da02343fb63a9d5a000d00f8fe8267fe2ca8808a
miloujb <emilyjbailey@hotmail.co.uk>
2019-08-27 13:36:43 +0100

    addComment partially written

--- src/components/newComment.js
+++ src/components/newComment.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import {postComment} from "./api"
 
 class AddComment extends Component {
     state = {
         newComment = '',
         isLoading = true
     }
+
+    componentDidMount() {
+        this.setState()
+    }
+
+    addComment = () => {
+        postComment(article_id).then(comment => {
+            this.setState({newComment: comment, isLoading: false})
+        })
+    }
+    
     render() {
         return (
             <div>
