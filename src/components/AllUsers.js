fc43cf9811a636039847dce29ae0471dc6b34313
miloujb <emilyjbailey@hotmail.co.uk>
2019-08-27 09:49:13 +0100

    AllUsers setup

---
+++ src/components/AllUsers.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class AllUsers extends Component {
+  state = {
+    users: null,
+    isLoading: true
+  };
+  render() {
+    return <div></div>;
+  }
+}
+
+export default AllUsers;
