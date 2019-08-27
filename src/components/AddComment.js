152bac5bddec0ce7fbba2d5513ceb3c766355413
miloujb <emilyjbailey@hotmail.co.uk>
2019-08-27 11:06:57 +0100

    addComment component setup

---
+++ src/components/AddComment.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AddComment extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default AddComment;
