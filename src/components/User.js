7a6991d116a948ba7696c12c7b710cb074a955d1
miloujb <emilyjbailey@hotmail.co.uk>
2019-08-27 09:02:46 +0100

    fetchUser function written

--- src/components/User.js
+++ src/components/User.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
+import { getUser } from "./api";
 
 class User extends Component {
   state = {
     user: null,
     isLoading: true
   };
+
+  componentDidMount() {
+    this.fetchUser();
+  }
+
+  fetchUser = () => {
+    getUser(username).then(username => {
+      this.setState({ user: username, isLoading: false });
+    });
+  };
+
   render() {
     return <div></div>;
   }
