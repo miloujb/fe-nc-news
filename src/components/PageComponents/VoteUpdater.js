ea29abffc7e8a3637dabc1b3b207c5a8c8fa3033
miloujb <emilyjbailey@hotmail.co.uk>
2019-08-28 23:13:59 +0100

    rolledback and tested

--- src/components/PageComponents/VoteUpdater.js
+++ src/components/PageComponents/VoteUpdater.js
@@ -1,16 +1,14 @@
 import React, { Component } from "react";
-import { patchVotes, patchCommentVotes } from "../API/api";
+import { patchVotes } from "../API/api";
 
 class VoteUpdater extends Component {
   state = {
     voteChanger: 0,
     article_id: this.props.article_id,
-    comment_id: this.props.comment_id,
     votes: this.props.votes
   };
 
   render() {
-    console.log(this.state);
     const { voteChanger, votes } = this.state;
     return (
       <div>
@@ -37,16 +35,9 @@ class VoteUpdater extends Component {
   changeVotes = event => {
     event.preventDefault();
     const { value } = event.target;
-    const { article_id, comment_id } = this.state;
+    const { article_id } = this.state;
     if (article_id) {
       patchVotes(article_id, value);
-      this.setState(currentState => {
-        return {
-          voteChanger: currentState.voteChanger + +value
-        };
-      });
-    } else if (comment_id) {
-      patchCommentVotes(comment_id, value);
       this.setState(currentState => {
         return {
           voteChanger: currentState.voteChanger + +value
