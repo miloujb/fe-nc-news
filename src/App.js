d34e89980ba5aa301dfacdb100d116cfdb674f6d
miloujb <emilyjbailey@hotmail.co.uk>
2019-08-29 09:42:12 +0100

    sorter added, bootstrap installed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 import Header from "./components/Header";
 import AllArticles from "./components/PageComponents/AllArticles";
 import AllTopics from "./components/PageComponents/AllTopics";
