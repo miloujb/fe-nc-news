036a749006d062bf10e650ba3d85d7460aca457e
miloujb <emilyjbailey@hotmail.co.uk>
2019-08-31 21:29:56 +0100

    bootstrap imported to index.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,49 +1,59 @@
 import React from "react";
+c
 
 function Header() {
   return (
-    <div className="header">
-      <nav class="navbar fixed-top navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="/">
-          NC NEWS
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="/">
-                Home <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/topics/football">
-                Football
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/topics/coding">
-                Coding
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/topics/cooking">
-                Cooking
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
+    <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+      <a className="navbar-brand" href="/">
+        NC NEWS
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarText"
+        aria-controls="navbarText"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarText">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="/">
+              Home <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/topics/football">
+              Football
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/topics/cooking">
+              Cooking
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/topics/coding">
+              Coding
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/topics/">
+              All Topics
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/articles/">
+              All Articles
+            </a>
+          </li>
+        </ul>
+        <span className="navbar-text">Logged in as: jessjelly</span>
+      </div>
+    </nav>
   );
 }
 
