fe1cbbef16544df96562ec719819deeb52984996
miloujb <emilyjbailey@hotmail.co.uk>
2019-09-02 12:00:05 +0100

    spinning gif on homepage

--- src/components/PageComponents/HomePage.js
+++ src/components/PageComponents/HomePage.js
@@ -3,14 +3,21 @@ import { Link } from "@reach/router";
 
 const HomePage = () => {
   return (
-    <div class="home-box">
-      <h2>Welcome to NC News!</h2>
-      <p>
-        Why not have a look at the <Link to="/topics">topics</Link> we have on
-        offer? Or the <Link to="/articles">articles</Link> we have in our
-        collection?
-      </p>
-    </div>
+    <>
+      <div class="home-box">
+        <h2>Welcome to NC News!</h2>
+        <p>
+          Why not have a look at the <Link to="/topics">topics</Link> we have on
+          offer? Or the <Link to="/articles">articles</Link> we have in our
+          collection?
+        </p>
+      </div>
+      <img
+        src="https://media3.giphy.com/media/y4dggMy4tUtPy/giphy.gif"
+        alt="spinning newspaper"
+        class="newspaper"
+      ></img>
+    </>
   );
 };
 
