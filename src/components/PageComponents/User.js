195f1e4d2b67aab9cb60f9559429a446c8b59a64
miloujb <emilyjbailey@hotmail.co.uk>
2019-09-16 08:37:07 +0100

    error incorporated for bad user

--- src/components/PageComponents/User.js
+++ src/components/PageComponents/User.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { getUser } from "../API/api";
 import Spinner from "../OtherComponents/Spinner";
+import Error from "../OtherComponents/Error";
 
 class User extends Component {
   state = {
     user: null,
-    isLoading: true
+    isLoading: true,
+    error: null
   };
 
   componentDidMount() {
@@ -14,14 +16,20 @@ class User extends Component {
 
   fetchUser = () => {
     const { username } = this.props;
-    getUser(username).then(username => {
-      this.setState({ user: username, isLoading: false });
-    });
+    getUser(username)
+      .then(username => {
+        this.setState({ user: username, isLoading: false });
+      })
+      .catch(error => {
+        const { status, statusText } = error.response;
+        this.setState({ error: { status, statusText }, isLoading: false });
+      });
   };
 
   render() {
-    const { user, isLoading } = this.state;
+    const { user, isLoading, error } = this.state;
     if (isLoading) return <Spinner />;
+    if (error) return <Error error={error} />;
     return (
       <div className="container">
         <h2>{user.username}'s Profile</h2>
